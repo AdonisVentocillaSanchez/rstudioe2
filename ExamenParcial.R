d1f81b2d6489b548a9b18647605bc8bec0bdd5c1
LUIS ALBERTO TOLEDO PAULINO <ltoledop@autonoma.edu.pe>
2020-06-14 21:43:02 -0500

    segundo cambios

--- ExamenParcial.R
+++ ExamenParcial.R
@@ -9,7 +9,7 @@ head(datos)
 datos$sex=as.factor(datos$sex)
 datos$ï..country=as.factor(datos$ï..country)
 datos$country.year=as.factor(datos$country.year)
-datos$age=as.factor(datos$age)
+datos$age=as.integer(datos$age)
 datos$generation=as.factor(datos$generation)
 datos$year=as.factor(datos$year)
 
@@ -19,14 +19,25 @@ datos=datos[,c(1:8,12)]
 
 
 summary(datos)
-
+#ï..country.-
+#year.-
+#sex.-
+#age.-
+#sucides_numero.-
+#poblation.-
+#tasa de suicidiio.-
+#pais por año.-
+#generacion.-
 
 str(datos)
 ########################
 #Exploración de datos
-hist(datos$year)
+hist(datos$suicides.100k.pop)
 
 install.packages("ggplot2")
 library(ggplot2)
 ggplot(data=datos)+
-  geom_histogram(mapping = aes(x=age, color=Mora))
+  geom_histogram(mapping = aes(x=suicides.100k.pop))
+
+
+boxplot(datos$suicides.100k.pop)
